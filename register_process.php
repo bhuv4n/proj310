64e18c5014fc309eb28b78a5fc246a2666a9546f
Bhuv4n <bspatel@student.concordia.ab.ca>
2023-11-30 05:23:59 -0700

    Update register_process.php

--- register_process.php
+++ register_process.php
@@ -1,19 +1,23 @@
 <?php
-include "db_connection.php";
+// register_process.php
+
+// Including your database connection file
+include("db_connection.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
-    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT); // Hash the password
     $email = $_POST["email"];
     $phone = $_POST["phone"];
 
+    // Checking if the username is not taken
     $check_query = "SELECT * FROM users WHERE username = '$username'";
     $check_result = mysqli_query($conn, $check_query);
 
     if (mysqli_num_rows($check_result) > 0) {
         echo "Username already taken. Please choose another.";
     } else {
-        // Insert user data into the 'users' table
+        // Inserting user data into the 'users' table
         $insert_query = "INSERT INTO users (username, password, email, phone) 
                         VALUES ('$username', '$password', '$email', '$phone')";
 
