21ef0c56d9e63c60af56251ece838afa4b99a890
Chirag-Rawat23 <131230998+Chirag-Rawat23@users.noreply.github.com>
2023-11-30 05:53:25 -0700

    _contact.php

--- _contact.php
+++ _contact.php
@@ -1,1 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1em;
+        }
+
+        section {
+            max-width: 600px;
+            margin: 50px auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h2 {
+            color: #333;
+        }
+
+        p {
+            line-height: 1.6;
+            color: #666;
+        }
+
+        address {
+            margin-top: 20px;
+            font-style: normal;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Contact Us</h1>
+    </header>
+
+    <section>
+        <h2>Fast Travel Agency</h2>
+        <p>Welcome to our contact page. Feel free to reach out to us using the following details:</p>
+
+        <address>
+            <p><strong>Address:</strong> 7128 Ada Blvd NW, Edmonton, AB T5B 4E4</p>
+            <p><strong>Email:</strong> fsttravel007@gmail.com</p>
+            <p><strong>Phone:</strong> +1 (555) 123-4567</p>
+        </address>
+    </section>
+</body>
+</html>
 
