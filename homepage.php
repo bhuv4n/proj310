75ae6e48acc7d7a7988b7f42e519dd0cca259715
Chirag-Rawat23 <131230998+Chirag-Rawat23@users.noreply.github.com>
2023-11-30 04:40:10 -0700

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Travel Agency</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: 'Arial', sans-serif;
+            overflow: hidden;
+            background: url('https://wallpapers.com/images/featured/rainy-nature-8djqv0eijjds6bk8.jpg') center/cover no-repeat fixed;
+            animation: backgroundAnimation 9s infinite;
+        }
+
+        @keyframes backgroundAnimation {
+            0% { background: url('https://wallpapers.com/images/featured/scenic-81a8evncb76xosaf.jpg') center/cover no-repeat fixed; }
+            33% { background: url('https://i.pinimg.com/originals/78/34/df/7834df02451f1f377a3bf9ac6d5f217b.jpg') center/cover no-repeat fixed; }
+            66% { background: url('https://wallpapers.com/images/hd/rain-nature-stones-up206c319tocfysa.jpg') center/cover no-repeat fixed; }
+            100% { background: url('https://wallpapers.com/images/featured/rainy-nature-8djqv0eijjds6bk8.jpg') center/cover no-repeat fixed; } 
+        }
+
+        header {
+            background-color: rgba(0, 0, 0, 0.7);
+            padding: 20px;
+            position: fixed;
+            width: 100%;
+            z-index: 1000;
+        }
+
+        nav {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+        }
+
+        nav a {
+            color: #fff;
+            text-decoration: none;
+            font-size: 14px;
+            font-weight: bold;
+        }
+
+        section {
+            padding: 100px 20px;
+            text-align: center;
+            color: #fff;
+        }
+        h1 {
+            font-family: poppins;
+            font-size: 36px; }
+    </style>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="#a">Home</a>
+            <a href="#b">Account</a>
+            <a href="#c">Contact</a>
+        </nav>
+    </header>
+
+    <section>
+        <h1>Fast Travel Agenct</h1>
+        <p>Welcome to an amazing journey around the world!</p>
+    </section>
+</body>
+</html>
