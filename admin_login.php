c81933b5042b459b68973f7d52287703066eb942
Bhuv4n <bspatel@student.concordia.ab.ca>
2023-11-30 05:28:21 -0700

    Add files via upload

---
+++ admin_login.php
@@ -0,0 +1,24 @@
+<!-- admin_login.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login</title>
+</head>
+<!-- Admin login  -->
+<body>
+    <h2>Admin Login</h2>
+    <form action="admin_login_process.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <br>
+        <input type="submit" value="Login">
+    </form>
+   
+</body>
+</html>
