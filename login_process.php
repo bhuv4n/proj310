95f95f7c941211e2844f6075a2b1a0ec78505a6e
Bhuv4n <bspatel@student.concordia.ab.ca>
2023-11-30 05:11:49 -0700

    Update login_process.php

--- login_process.php
+++ login_process.php
@@ -1,26 +1,31 @@
 <?php
-include "db_connection.php";
+// login_process.php
+
+// Including your database connection file
+include("db_connection.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
+
+    // Checking user credentials
     $login_query = "SELECT * FROM users WHERE username = '$username'";
     $login_result = mysqli_query($conn, $login_query);
 
     if (mysqli_num_rows($login_result) > 0) {
         $user_data = mysqli_fetch_assoc($login_result);
+
+        // Verifying the password
         if (password_verify($password, $user_data["password"])) {
             session_start();
             $_SESSION["user_id"] = $user_data["id"];
             $_SESSION["username"] = $user_data["username"];
             header("Location: customer_choice.php");
         } else {
-            echo "Credentials do not match our records.";
-	    header("Location: contact.php");
+            echo "Invalid password. Please try again.";
         }
     } else {
-        echo "User not found. New registration required.";
-	header("Location: register.php");
+        echo "User not found. Please register.";
     }
 
     mysqli_close($conn);
