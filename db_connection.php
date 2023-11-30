e31a822416e10884da4b014c0d2b61d22dc71e8a
Bhuv4n <bspatel@student.concordia.ab.ca>
2023-11-30 05:22:34 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,15 @@
 <?php
+// db_connection.php
+
 $servername = "localhost";
 $username = "root";
 $password = "Bhuvan@11";
 $database = "fast_travel";
+
+// Creating connection
 $conn = mysqli_connect($servername, $username, $password, $database);
+
+// to Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
